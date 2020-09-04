2daeafe2942d45a5ccd1d4b28ecf949e7613448d
Luu Han Tai <hantai@jv-it.com.vn>
2020-09-04 14:02:23 +0700

    update link menu auto

--- assets/script/layout.js
+++ assets/script/layout.js
@@ -1,26 +1,36 @@
+var locationHref = location.href,
+locationOrigin = location.origin,
+folderName = 'vinachaki-html',
+locationPage = locationHref.replace(locationOrigin,''),
+locationPageFolder = folderName != '' ? locationPage.replace('/'+folderName+'/','') : '';
+arrayHref = locationPageFolder == '' ? 0 : locationPageFolder.split('/'),
+linkHref = '';
+for(var i = 0; i < arrayHref.length - 1 ; i++){
+    linkHref = linkHref + '../';
+}
 
 var header = `<nav class="navbar navbar-expand-lg">
 <div class="container">
-    <a class="navbar-logo" href="#"><img src="assets/images/index/logo.png" alt=""></a>
+    <a class="navbar-logo" href="#"><img src="`+linkHref+`assets/images/index/logo.png" alt=""></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-                <a class="nav-link" href="#">HOME <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="`+linkHref+`">HOME <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">WHY CHOOSE US?</a>
+                <a class="nav-link" href="`+linkHref+`">WHY CHOOSE US?</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">PRODUCTS</a>
+                <a class="nav-link" href="`+linkHref+`products/">PRODUCTS</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">FAQ</a>
+                <a class="nav-link" href="`+linkHref+`faq.html">FAQ</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">CONTACT US</a>
+                <a class="nav-link" href="`+linkHref+`contact.html">CONTACT US</a>
             </li>
         </ul>
     </div>
@@ -33,7 +43,7 @@ footer = `
     <div class="footerWrap">
         <div class="footer_logo">
             <div class="footer_logoImg">
-                <img src="assets/images/index/logo_footer.png" alt="">
+                <img src="`+linkHref+`assets/images/index/logo_footer.png" alt="">
             </div>
             <p class="footer_logoDesc">We are charcoal manufacturer, charcoal<br>distributor and charcoal exporter. We are one of<br>leading exporters of wood charcoal in Viet Nam.<br>We supply charcoal for barbecue (BBQ), charcoal<br> for Shisha, charcoal for industrial applications.</p>
         </div>
@@ -41,14 +51,14 @@ footer = `
             <div class="footer_menuItem">
                 <ul>
                     <li>
-                        <a href="">product</a>
+                        <a href="`+linkHref+`products/">product</a>
                         <ul>
-                            <li><a href="">White charcoal</a></li>
-                            <li><a href="">Hardwood charcoal</a></li>
-                            <li><a href="">Softwood charcoal</a></li>
-                            <li><a href="">Mix wood charcoal</a></li>
-                            <li><a href="">Coconut charcoal</a></li>
-                            <li><a href="">Sawduct charcoal</a></li>
+                            <li><a href="`+linkHref+`products/white-charcoal.html">White charcoal</a></li>
+                            <li><a href="`+linkHref+`">Hardwood charcoal</a></li>
+                            <li><a href="`+linkHref+`">Softwood charcoal</a></li>
+                            <li><a href="`+linkHref+`">Mix wood charcoal</a></li>
+                            <li><a href="`+linkHref+`">Coconut charcoal</a></li>
+                            <li><a href="`+linkHref+`">Sawduct charcoal</a></li>
                         </ul>
                     </li>
                 </ul>
@@ -56,11 +66,11 @@ footer = `
             <div class="footer_menuItem">
                 <ul>
                     <li>
-                        <a href="">help</a>
+                        <a href="`+linkHref+`">help</a>
                         <ul>
-                            <li><a href="">Customer support</a></li>
-                            <li><a href="">Contact</a></li>
-                            <li><a href="">FAQ</a></li>
+                            <li><a href="`+linkHref+`">Customer support</a></li>
+                            <li><a href="`+linkHref+`">Contact</a></li>
+                            <li><a href="`+linkHref+`faq.html">FAQ</a></li>
                         </ul>
                     </li>
                 </ul>
@@ -84,9 +94,9 @@ footer = `
                     <li>
                         <a href="">legal</a>
                         <ul>
-                            <li><a href="">Tearms of use</a></li>
-                            <li><a href="">Privacy policy</a></li>
-                            <li><a href="">Legal notice</a></li>
+                            <li><a href="`+linkHref+`">Tearms of use</a></li>
+                            <li><a href="`+linkHref+`">Privacy policy</a></li>
+                            <li><a href="`+linkHref+`">Legal notice</a></li>
                         </ul>
                     </li>
                 </ul>
